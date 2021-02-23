1bbd14711fd62ccfe27407282af9422d8db48dd7
pgg-dev <park.geumgyu@gmail.com>
2021-02-24 05:35:04 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,181 @@
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        // != null : value.length가 0이라도 false
+        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value > validatableInput.min;
+    }
+
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value < validatableInput.max;
+    }
+
+    return isValid;
+}
+
+// autobind decorator
+function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content, true
+        );
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+
+    }
+}
+
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content, true
+        );
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+
+        this.configure();
+        this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const entereDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        }
+
+        const descriptionValidatable: Validatable = {
+            value: entereDescription,
+            required: true,
+            minLength: 5,
+            min: 1,
+            max: 5
+        }
+
+        const peopleValidatable: Validatable = {
+            value: enteredPeople,
+            required: true
+        }
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, entereDescription, +enteredPeople];
+        }
+    }
+
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        // console.log(this.titleInputElement.value);
+        // this를 binding 시켜줘야 출력됨
+
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+
+        // this.element.addEventListener('submit', this.submitHandler.bind(this));
+        // 이벤트 함수가 호출되면 this가 바뀌기 때문에 bind 사용해서 class의 this를 넘겨줌
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInpt = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
+
+
+// console.log(prjInpt)
