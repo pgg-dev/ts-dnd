0b538cc07da2f3233f924b5ca28146cc5afae592
pgg-dev <park.geumgyu@gmail.com>
2021-03-10 05:22:20 +0900

    project type filtering and list item init

--- src/app.ts
+++ src/app.ts
@@ -115,9 +115,14 @@ class ProjectList {
         this.element.id = `${this.type}-projects`; // section
 
         projectState.addListner((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
-
         });
 
 
@@ -127,6 +132,7 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
